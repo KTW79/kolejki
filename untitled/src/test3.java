a2b154ca0d8ab48d5083aa8969145a83f5555c63
Kasia <kasia@wiseman.pl>
2016-12-15 19:00:32 +0100

    testuje wysyłanie na git

--- untitled/src/test3.java
+++ untitled/src/test3.java
@@ -3,16 +3,19 @@
  */
 public class test3 {
     public static void main(String[] args) {
-        System.out.println("Liczba int: ");
-        System.out.println(25);
-
-        System.out.println("Liczba double: ");
-        System.out.println(25.75);
-
-        System.out.println("Znak A: ");
-        System.out.println('A');
-
-        System.out.print("Czy uczę się programowania? ");
+        System.out.print("Cześć Jan, słyszałem, że masz ");
+        System.out.print(25);
+        System.out.println(" lat i uczysz się programowania. Czy to prawda?");
+        System.out.println(true);
+        System.out.print("Czy Twoje inicjały to ");
+        System.out.print('J');
+        System.out.print(" ");
+        System.out.print('K');
+        System.out.println('?');
+        System.out.print("Tak, ");
         System.out.println(true);
     }
 }
+
+
+
